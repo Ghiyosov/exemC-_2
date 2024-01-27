39f3db657c4a6c9dc2adbe14042712fe4bb1f7e0
Ghiyosov <154344895+Ghiyosov@users.noreply.github.com>
2024-01-27 17:58:00 +0500

    Update Program.cs

--- -tast_1/Program.cs
+++ -tast_1/Program.cs
@@ -1,1 +1,1 @@
-﻿Console.WriteLine("OOP Objekt oreitir programing: 1.Encapsulation 2.Inherton 2.Polifarmizm 3.Abstraksiya,  ba oyektho va classho judo meshavad");
+﻿Console.WriteLine("OOP Objekt oreitir programing: 1.Encapsulation 2.Inherton 2.Polifarmizm 3.Abstraksiya,  ba obyektho va classho judo meshavad");
